3a2a922f892b1831ab784975645b927ef6ac4b94
Thiago “Fred” Porciúncula <tfcporciuncula@gmail.com>
2018-08-25 10:38:40 +0200

    Use data binding for the list

--- app/src/main/java/com/blinkist/easylibrary/library/LibraryAdapter.kt
+++ app/src/main/java/com/blinkist/easylibrary/library/LibraryAdapter.kt
@@ -1,27 +1,12 @@
 package com.blinkist.easylibrary.library
 
-import android.support.v7.recyclerview.extensions.ListAdapter
 import android.support.v7.util.DiffUtil
-import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
 import com.blinkist.easylibrary.R
+import com.blinkist.easylibrary.base.DataBindingAdapter
 import com.blinkist.easylibrary.model.Book
-import com.blinkist.easylibrary.model.WeekSection
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
-import kotlinx.android.synthetic.main.item_book.view.*
 import javax.inject.Inject
 
-class LibraryAdapter @Inject constructor() :
-    ListAdapter<Librariable, LibraryAdapter.ViewHolder>(DiffCallback()) {
-
-    companion object {
-        private const val ITEM_VIEW_TYPE_BOOK = R.layout.item_book
-        private const val ITEM_VIEW_TYPE_SECTION = R.layout.item_section
-    }
+class LibraryAdapter @Inject constructor() : DataBindingAdapter<Librariable>(DiffCallback()) {
 
     class DiffCallback : DiffUtil.ItemCallback<Librariable>() {
 
@@ -35,45 +20,9 @@ class LibraryAdapter @Inject constructor() :
         }
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        fun bindBook(book: Book) {
-            itemView.apply {
-                titleTextView.text = book.title
-                authorsTextView.text = book.authors
-                publishedDateTextView.text = book.publishedDate
-
-                Glide.with(coverImageView)
-                    .load(book.thumbnail)
-                    .transition(withCrossFade())
-                    .into(coverImageView)
-            }
-        }
-
-        fun bindWeekSection(weekSection: WeekSection) {
-            val title = itemView.context.getString(
-                R.string.week_section_title, weekSection.initialDate, weekSection.finalDate
-            )
-            (itemView as TextView).text = title
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val librariable = getItem(position)
-        when (librariable) {
-            is Book -> holder.bindBook(librariable)
-            is WeekSection -> holder.bindWeekSection(librariable)
-        }
-    }
-
     override fun getItemViewType(position: Int) = if (getItem(position) is Book) {
-        ITEM_VIEW_TYPE_BOOK
+        R.layout.item_book
     } else {
-        ITEM_VIEW_TYPE_SECTION
+        R.layout.item_section
     }
 }
