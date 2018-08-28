244cb3518d208c46e41b847c8ebf61bd580a598d
Thiago “Fred” Porciúncula <tfcporciuncula@gmail.com>
2018-08-28 15:25:14 +0200

    Makes things simpler on Retrofit initialization

--- app/src/androidTest/java/com/blinkist/easylibrary/di/RetrofitTestModule.kt
+++ app/src/androidTest/java/com/blinkist/easylibrary/di/RetrofitTestModule.kt
@@ -4,7 +4,6 @@ import com.blinkist.easylibrary.service.BooksService
 import dagger.Module
 import dagger.Provides
 import io.appflate.restmock.RESTMockServer
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -16,7 +15,6 @@ object RetrofitTestModule {
     @JvmStatic @Provides @Singleton
     fun provideBooksService(): BooksService = Retrofit.Builder()
         .baseUrl(RESTMockServer.getUrl())
-        .client(OkHttpClient.Builder().build())
         .addConverterFactory(MoshiConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
--- app/src/main/java/com/blinkist/easylibrary/di/ServiceInjection.kt
+++ app/src/main/java/com/blinkist/easylibrary/di/ServiceInjection.kt
@@ -4,9 +4,6 @@ import com.blinkist.easylibrary.service.BooksService
 import dagger.Module
 import dagger.Provides
 import io.appflate.restmock.RESTMockServer
-import okhttp3.OkHttpClient
-import retrofit2.CallAdapter
-import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -22,7 +19,6 @@ object RetrofitModule {
     @JvmStatic @Provides @Singleton
     fun provideBooksService(): BooksService = Retrofit.Builder()
         .baseUrl(RESTMockServer.getUrl())
-        .client(OkHttpClient.Builder().build())
         .addConverterFactory(MoshiConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
