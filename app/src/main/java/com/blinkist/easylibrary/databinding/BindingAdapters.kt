8a582a7f0aa8b71e3e99022ce7bbebd2d569f2b5
Fred Porciúncula <fred@blinkist.com>
2019-12-05 00:10:19 +0100

    Simplify data binding in the bottom sheet
    With a new binding adapter.

--- app/src/main/java/com/blinkist/easylibrary/databinding/BindingAdapters.kt
+++ app/src/main/java/com/blinkist/easylibrary/databinding/BindingAdapters.kt
@@ -1,6 +1,8 @@
 package com.blinkist.easylibrary.databinding
 
+import android.view.View
 import android.widget.ImageView
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.bumptech.glide.Glide
@@ -17,4 +19,9 @@ fun loadImage(view: ImageView, imageUrl: String) {
 @BindingAdapter("onRefresh")
 fun setOnRefreshListener(view: SwipeRefreshLayout, listener: () -> Unit) {
   view.setOnRefreshListener { listener() }
+}
+
+@BindingAdapter("isVisible")
+fun setVisibility(view: View, isVisible: Boolean) {
+  view.isVisible = isVisible
 }
