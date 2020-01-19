ac2b565e7ebc61eb6d99d7153739f29a3e99caf1
Fred Porciúncula <fred@blinkist.com>
2020-01-19 11:34:49 +0100

    Webview nits

--- app/src/main/java/com/blinkist/easylibrary/features/webview/WebViewActivity.kt
+++ app/src/main/java/com/blinkist/easylibrary/features/webview/WebViewActivity.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.webkit.WebChromeClient
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -17,7 +15,7 @@ import com.blinkist.easylibrary.ktx.unsyncLazy
 class WebViewActivity : AppCompatActivity() {
 
   companion object {
-    private const val extraUrl = "com.blinkist.easylibrary.extraUrl"
+    private const val extraUrl = "com.blinkist.easylibrary.features.webview.extraUrl"
 
     fun newIntent(context: Context, url: String) = context.newIntent<WebViewActivity> {
       putExtra(extraUrl, url)
@@ -32,35 +30,27 @@ class WebViewActivity : AppCompatActivity() {
     setupWebView()
   }
 
-  private fun setupWebView() = with(binding.webView) {
-    webViewClient = object : WebViewClient() {
+  private fun setupWebView() = with(binding) {
+    webView.webViewClient = object : WebViewClient() {
       override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
-        binding.progressBarContainer.isVisible = true
+        progressBarContainer.isVisible = true
       }
 
       override fun onPageFinished(view: WebView, url: String) {
-        binding.progressBarContainer.isVisible = false
-      }
-
-      override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
-        // TODO
+        progressBarContainer.isVisible = false
       }
     }
 
-    webChromeClient = object : WebChromeClient() {
+    webView.webChromeClient = object : WebChromeClient() {
       override fun onProgressChanged(view: WebView, newProgress: Int) {
-        binding.progressBar.progress = newProgress
+        progressBar.progress = newProgress
       }
     }
 
-    loadUrl(intent.getStringExtra(extraUrl))
+    webView.loadUrl(intent.getStringExtra(extraUrl))
   }
 
-  override fun onBackPressed() {
-    if (binding.webView.canGoBack()) {
-      binding.webView.goBack()
-    } else {
-      super.onBackPressed()
-    }
+  override fun onBackPressed() = with(binding.webView) {
+    if (canGoBack()) goBack() else super.onBackPressed()
   }
 }
