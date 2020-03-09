cc649bdf4b09d4ba094becd232e70f59016b1404
Fred Porciúncula <fred@blinkist.com>
2020-03-09 23:41:22 +0100

    Add extensions to deal with saved state view models

--- util/src/main/java/com/blinkist/easylibrary/util/ktx/LazyViewModels.kt
+++ util/src/main/java/com/blinkist/easylibrary/util/ktx/LazyViewModels.kt
@@ -7,29 +7,58 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 inline fun <reified T : ViewModel> FragmentActivity.lazyViewModel(
-  crossinline provider: () -> T
+  crossinline viewModelProducer: () -> T
 ) = viewModels<T> {
   object : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>) = provider() as T
+    override fun <T : ViewModel> create(modelClass: Class<T>) = viewModelProducer() as T
+  }
+}
+
+inline fun <reified T : ViewModel> FragmentActivity.lazySavedStateViewModel(
+  crossinline viewModelProducer: (handle: SavedStateHandle) -> T
+) = viewModels<T> {
+  object : AbstractSavedStateViewModelFactory(this, intent.extras) {
+    override fun <T : ViewModel> create(key: String, modelClass: Class<T>, handle: SavedStateHandle) =
+      viewModelProducer(handle) as T
   }
 }
 
 inline fun <reified T : ViewModel> Fragment.lazyViewModel(
-  crossinline provider: () -> T
+  crossinline viewModelProducer: () -> T
 ) = viewModels<T> {
   object : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>) = provider() as T
+    override fun <T : ViewModel> create(modelClass: Class<T>) = viewModelProducer() as T
+  }
+}
+
+inline fun <reified T : ViewModel> Fragment.lazySavedStateViewModel(
+  crossinline viewModelProducer: (handle: SavedStateHandle) -> T
+) = viewModels<T> {
+  object : AbstractSavedStateViewModelFactory(this, requireArguments()) {
+    override fun <T : ViewModel> create(key: String, modelClass: Class<T>, handle: SavedStateHandle) =
+      viewModelProducer(handle) as T
   }
 }
 
 inline fun <reified T : ViewModel> Fragment.lazyActivityViewModel(
-  crossinline provider: () -> T
+  crossinline viewModelProducer: () -> T
 ) = activityViewModels<T> {
   object : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>) = provider() as T
+    override fun <T : ViewModel> create(modelClass: Class<T>) = viewModelProducer() as T
+  }
+}
+
+inline fun <reified T : ViewModel> Fragment.lazySavedStateActivityViewModel(
+  crossinline viewModelProducer: (handle: SavedStateHandle) -> T
+) = activityViewModels<T> {
+  object : AbstractSavedStateViewModelFactory(this, requireArguments()) {
+    override fun <T : ViewModel> create(key: String, modelClass: Class<T>, handle: SavedStateHandle) =
+      viewModelProducer(handle) as T
   }
 }
